7337aef35f11d6d495db7a2ab5e8bd7743f19cc8
MarcKruzik <marc.kruzik@gmail.com>
2023-01-06 22:07:14 +0100

    Namer Prefix

---
+++ code/Datour/Namer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Datour
+{
+    public class Namer
+    {
+        const string PREFIX_INVALID = "";
+
+        public static string from_dateonly_get_ymd(DateOnly dateonly)
+        {
+            return $"{dateonly.Year}_{dateonly.Month:D2}_{dateonly.Day:D2}";
+        }
+
+        public static string from_dateonly_get_prefix(DateOnly dateonly)
+        {
+            string ymd = from_dateonly_get_ymd(dateonly);
+            return $"{ymd}-";
+        }
+
+        public static string from_filename_get_prefix(string filename)
+        {
+            Regex regex = new Regex(@"^\d\d\d\d_\d\d_\d\d-");
+            Match m = regex.Match(filename);
+            if (m.Success == false)
+            {
+                return PREFIX_INVALID;
+            }
+            return m.Groups[0].Value;
+        }
+
+        public static bool from_filename_has_prefix(string filename)
+        {
+            string prefix = from_filename_get_prefix(filename);
+            return prefix != PREFIX_INVALID;
+        }
+    }
+}
--- code/TestDatour/UnitTest1.cs
+++ code/TestDatour/UnitTest1.cs
@@ -1,16 +1,79 @@
+using Datour;
+
 namespace TestDatour
 {
     public class Tests
     {
+        const string FILENAME = "filename.ext";
+
         [SetUp]
         public void Setup()
         {
+            File.Delete(FILENAME);
         }
 
         [Test]
-        public void Test1()
+        public void Filepath_Exist()
         {
-            Assert.Pass();
+            File.Create(FILENAME).Close();
+            Assert.That(File.Exists(FILENAME), Is.True);
+        }
+
+        [Test]
+        public void Filepath_ExistNot()
+        {
+            Assert.That(File.Exists(FILENAME), Is.False);
+        }
+        
+        [TestCase(2022, 1, 1, "2022_01_01")]
+        [TestCase(2022, 12, 31, "2022_12_31")]
+        [TestCase(2022, 1, 31, "2022_01_31")]
+        [TestCase(2022, 12, 1, "2022_12_01")]
+        public void from_dateonly_get_ymd(int year, int month, int day, string expected_ymd)
+        {
+            DateOnly dateonly = new DateOnly(year, month, day);
+            string ymd = Datour.Namer.from_dateonly_get_ymd(dateonly);
+            Assert.That(ymd, Is.EqualTo(expected_ymd));
+        }
+
+        [TestCase(2022, 1, 2, "2022_01_02-")]
+        public void from_dateonly_get_prefix(int year, int month, int day, string expected_prefix)
+        {
+            DateOnly dateonly = new DateOnly(year, month, day);
+            string prefix = Datour.Namer.from_dateonly_get_prefix(dateonly);
+            Assert.That(prefix, Is.EqualTo(expected_prefix));
+        }
+
+
+        [TestCase("2022_01_02-filename.ext", "2022_01_02-")]
+        public void from_filename_get_prefix(string filename, string expected_prefix)
+        {
+            string prefix = Datour.Namer.from_filename_get_prefix(filename);
+            Assert.That(prefix, Is.EqualTo(expected_prefix));
+        }
+        
+        [TestCase("2022_01_02-filename.ext")]
+        public void from_filename_has_prefix(string filename)
+        {
+            bool has_prefix = Datour.Namer.from_filename_has_prefix(filename);
+            Assert.That(has_prefix, Is.True);
+        }
+        
+        [TestCase("filename.ext")]
+        [TestCase("2022-filename.ext")]
+        [TestCase("2022_01-filename.ext")]
+        [TestCase("2022_01_02filename.ext")]
+        [TestCase("2022_01_02_filename.ext")]
+        [TestCase("2022_01_02.ext")]
+        [TestCase("2022-01-02-filename.ext")]
+        [TestCase("2022_01_2-filename.ext")]
+        [TestCase("2022_1_02-filename.ext")]
+        [TestCase("2022_1_2-filename.ext")]
+        [TestCase("2022_01_002-filename.ext")]
+        public void from_filename_has_prefix_false(string filename)
+        {
+            bool has_prefix = Datour.Namer.from_filename_has_prefix(filename);
+            Assert.That(has_prefix, Is.False);
         }
     }
 }
